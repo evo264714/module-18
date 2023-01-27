2827f570146badd649de45fd80796edf5504b3d0
evo264714 <mh264714@gmail.com>
2023-01-27 15:16:33 +0600

    first

---
+++ array.js
@@ -0,0 +1,11 @@
+var friendsAge = [11, 15, 25, 17];
+
+
+
+var picnicFee = [5000, 2000, 4000, 3000];
+
+var names = ['messi', 'neymar', 'ronaldo', 'rooney'];
+
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+
+console.log(names.length);
---
+++ variable.js
@@ -0,0 +1,30 @@
+//* Number type variable
+var sunglassPrice = 120; 
+
+//* String type variable
+var name = "Evo";
+
+//* Boolean type variable
+var isTrue = true;
+
+//* Two types of numbers: Integer: 120;
+//* float, decimal: 3.65
+
+//* Variable naming convension: rules, best practice
+/*
+*1. Single word
+*2. no gap or space
+*3. no quote
+*4. no keyword
+*5. cannot contain dash
+*7. should use camelCase
+
+*/
+
+//* Math Opererations: +, -, *, /
+//* shorthand: +=, -=, ++, --
+
+//*Convert: parseINT, parseFloat
+
+//* toFixed
+//* remainder: vagses: %
