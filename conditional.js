001f476f48a7e74ada1739f357a5d37681bbc7b9
evo264714 <mh264714@gmail.com>
2023-01-27 16:28:57 +0600

    last

---
+++ conditional.js
@@ -0,0 +1,18 @@
+var iphonePrice = 79000;
+var myBudget = 95000;
+// If iphone price is less than my budget. i will buy the iPhone.
+
+
+if (iphonePrice < myBudget){
+    console.log('I will buy');
+}
+
+var chickenPrice = 500;
+var myMoney = 50;
+
+if(chickenPrice < myMoney){
+    console.log('I will eat Chicken');
+}
+else{
+    console.log('I will not eat chicken');
+}
